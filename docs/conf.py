c6d54edeb604573876f4ce316bbd318d64aaf33f
BenjMy <36472081+BenjMy@users.noreply.github.com>
2021-10-25 20:01:21 +0200

    chnage kernel

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ extensions = [
 ]
 
 nbsphinx_execute = 'always'
-nbsphinx_kernel_name = 'Dimech'
+#nbsphinx_kernel_name = 'Dimech'
 
 # Produce pages for each class and function
 autosummary_generate = True
