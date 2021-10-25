8be3162a7a094bdd13e2f12199bbfe88f85349d1
BenjMy <36472081+BenjMy@users.noreply.github.com>
2021-10-25 12:02:20 +0200

    sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -103,21 +103,21 @@ html_theme = 'sphinx_rtd_theme'
 # html_sidebars = {}
 
 
-html_favicon = 'favicon.svg'
-html_title = project + ' version ' + release
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+# html_favicon = 'img/logo.png'
+# html_title = project + ' version ' + release
+# html_theme_path = guzzle_sphinx_theme.html_theme_path()
+# html_theme = 'guzzle_sphinx_theme'
 
-html_theme_options = {
-    'navigation_with_keys': True,
-    'project_nav_name': project,
-    'base_url': 'https://nbsphinx.readthedocs.io/en/guzzle-theme/',
-    'globaltoc_depth': 3,
-    #'globaltoc_collapse': False,
-}
-html_sidebars = {
-    '**': ['logo-text.html', 'globaltoc.html', 'searchbox.html']
-}
+# html_theme_options = {
+#     'navigation_with_keys': True,
+#     'project_nav_name': project,
+#     'base_url': 'https://nbsphinx.readthedocs.io/en/guzzle-theme/',
+#     'globaltoc_depth': 3,
+#     #'globaltoc_collapse': False,
+# }
+# html_sidebars = {
+#     '**': ['logo-text.html', 'globaltoc.html', 'searchbox.html']
+# }
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
